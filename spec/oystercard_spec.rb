8ff5767d53f9a41de11ef717aa31ffb7f3ccad5b
Aday Mesa <adaymesagc@gmail.com>
2016-04-20 13:02:32 +0100

    error fixed / challenge 10

--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -23,45 +23,47 @@ describe Oystercard do
 	end
   
   context "contact" do
+	    it "has contact" do
 	    balance_limit = Oystercard::BALANCE_LIMIT
 	    subject.top_up(balance_limit)
 	    expect { subject.top_up 1}.to raise_error "Your balance cannot exceed £#{balance_limit}"
 	  end
+	 end
 
-  end
 
+	describe "Touching in and out" do
 
-describe "Touching in and out" do
+	  context "Card has enough money" do
 
-  context "Card has enough money" do
+	    before do
+	      subject.top_up(Oystercard::MINIMUM_BALANCE)
+	    end
 
-    before do
-      subject.top_up(Oystercard::MINIMUM_BALANCE)
-    end
+			it "touches card in" do
+				subject.touch_in
+				expect(subject).to be_in_journey
+			end
 
-		it "touches card in" do
-			subject.touch_in
-			expect(subject).to be_in_journey
+	    it "touches card out" do
+	      subject.touch_in
+	      subject.touch_out
+	      expect(subject).not_to be_in_journey
+	    end
+
+			it "confirms user is journey" do
+				expect(subject).not_to be_in_journey
+			end
+
+	  end
+
+	  context "No money on the card" do
+
+	    it "Does not allow travel below minimum balance" do
+	      expect {subject.touch_in}.to raise_error "You do not have the minimum balance for travel"
+	    end
 		end
-
-    it "touches card out" do
-      subject.touch_in
-      subject.touch_out
-      expect(subject).not_to be_in_journey
-    end
-
-		it "confirms user is journey" do
-			expect(subject).not_to be_in_journey
-		end
-
-  end
-
-  context "No money on the card" do
-
-    it "Does not allow travel below minimum balance" do
-      expect {subject.touch_in}.to raise_error "You do not have the minimum balance for travel"
-    end
 	end
 end
-end
+
+
 
